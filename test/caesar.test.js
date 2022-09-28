b26a115f5b261bb2b6bc09d7577a88cb6bc5baac
Katie Gaines <klgaines18@gmail.com>
2022-09-28 06:05:10 -0400

    added testing for shift = 0

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,10 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { caesar } = require("../src/caesar");
+
+describe("caesar", () => {
+    it("should return false if shift value is equal to 0", () => {
+        const actual = caesar("message", 0);
+        expect(actual).to.be.false;
+    });
+});
