d9925ce4501c490e7c058feb63c667a6207cb707
Katie Gaines <klgaines18@gmail.com>
2022-09-28 09:55:39 -0400

    add test to check if absence of alphabet returns false

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,12 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { substitution } = require("../src/substitution");
+
+describe("substitution", () => {
+    describe("error handling", () => {
+        it("should return false if alphabet is not given", () => {
+            const actual = substitution("message");
+            expect(actual).to.be.false;
+        });
+    });
+});
