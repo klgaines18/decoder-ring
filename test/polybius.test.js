188b1c75056f8ac46dc78537dd19e87425d453a3
Katie Gaines <klgaines18@gmail.com>
2022-09-28 09:17:57 -0400

    added test to check if result is a string

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,12 @@
 // Write your tests here!
+const { expect } = require("chai");
+const { polybius } = require("../src/polybius");
+
+describe("polybius", () => {
+    describe("encoding", () => {
+        it("should return a value of the type 'string'", () => {
+            actual = polybius("message");
+            expect(actual).to.be.a('string')
+        });
+    });
+});
