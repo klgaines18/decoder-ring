486833c3a95122c1a5e7b88b422f00790ca8d57c
Katie Gaines <klgaines18@gmail.com>
2022-09-28 10:14:17 -0400

    added encoding tests

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -23,4 +23,30 @@ describe("substitution", () => {
             const actual = substitution("thinkful", "abcabcabcabcabcabcabcabcyz")
         });
     });
+
+    describe("encoding", () => {
+        it("should correctly translate a message based on the given alphabet", () => {
+            const actual = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+            const expected = "jrufscpw";
+            expect(actual).to.equal(expected);
+        });
+
+        it("should leave spaces as is", () => {
+            const actual = substitution("You are an excellent spy", "xoyqmcgrukswaflnthdjpzibev");
+            const expected = "elp xhm xf mbymwwmfj dne";
+            expect(actual).to.equal(expected);
+        });
+
+        it("should allow a submitted alphabet to contain special characters", () => {
+            const actual = substitution("message", "$wae&zrdxtfcygvuhbijnokmpl");
+            const expected = "y&ii$r&";
+            expect(actual).to.equal(expected);
+        });
+
+        it("should ignore capital letters", () => {
+            const lower = substitution("thinkful", "xoyqmcgrukswaflnthdjpzibev");
+            const upper = substitution("ThInKfUl", "xoyqmcgrukswaflnthdjpzibev");
+            expect(lower).to.equal(upper);
+        });
+    });
 });
