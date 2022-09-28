dc3a0d47b96735adb126a2b56851542117c21944
Katie Gaines <klgaines18@gmail.com>
2022-09-28 09:41:50 -0400

    added decoding tests

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -33,4 +33,29 @@ describe("polybius", () => {
             expect(actual).to.equal(expected);
         });
     });
+
+    describe("decoding", () => {
+        it("should return false if length of input is odd", () => {
+            const actual = polybius("325113134", false);
+            expect(actual).to.be.false;
+        });
+
+        it("should encode a message by translating each coordinate pair into a letter", () => {
+            const actual = polybius("3251131343", false);
+            const expected = "hello";
+            expect(actual).to.equal(expected);
+        });
+
+        it("should translate 42 into '(i/j)'", () => {
+            const actual = polybius("4432423352125413", false);
+            const expected = "th(i/j)nkful";
+            expect(actual).to.equal(expected);
+        });
+
+        it("should leave spaces as is", () => {
+            const actual = polybius("3251131343 2543241341", false);
+            const expected = "hello world";
+            expect(actual).to.equal(expected);
+        });
+    });
 });
