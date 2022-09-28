85e9e93f3e07b2165cce82e51c5110de54547e2c
Katie Gaines <klgaines18@gmail.com>
2022-09-28 09:22:42 -0400

    added test to check proper encoding to coordinate pairs

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -5,8 +5,14 @@ const { polybius } = require("../src/polybius");
 describe("polybius", () => {
     describe("encoding", () => {
         it("should return a value of the type 'string'", () => {
-            actual = polybius("message");
-            expect(actual).to.be.a('string')
+            const actual = polybius("message");
+            expect(actual).to.be.a('string');
+        });
+
+        it("should encode a message by translating each letter into a coordinate pair", () => {
+            const actual = polybius("thinkful");
+            const expected = "4432423352125413";
+            expect(actual).to.equal(expected);
         });
     });
 });
